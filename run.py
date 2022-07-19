aa61a9f213ee8f86159df7b243297d1cab1d70db
oks-erm <oks.erm.012@gmail.com>
2022-07-19 22:43:43 +0000

    Wire up APIs

--- run.py
+++ run.py
@@ -1,3 +1,19 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import gspread
+from google.oauth2.service_account import Credentials
+
+SCOPE = [ 
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CRED = Credentials.from_service_account_file('cred.json')
+SCOPED_CRED = CRED.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CRED)
+SHEET = GSPREAD_CLIENT.open('love_sandwiches')
+
+sales = SHEET.worksheet('sales')
+
+data = sales.get_all_values()
+
+print(data)
